15ce3721706a9d3b69474661cdc100bc476b1a95
JagjitBhatia-UIC <jbhati3@uic.edu>
2020-11-26 03:00:26 -0600

    Updated Caravan class with comments and minor adjustments

--- Caravan.java
+++ Caravan.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
+// This object holds the player's inventory of gems
 public class Caravan {
-  private Gem[] inventory;
-  private ArrayList<Gem> excess;  // Must be cleared by end of turn
-  private int size;
+  private Gem[] inventory;  // Consists of 10 slots, each of which can contain up to 1 gem
+  private ArrayList<Gem> excess;  // MUST BE CLEARED BEFORE TURN ENDS
+  private int size; // Number of slots filled
 
   public Caravan() {
     inventory = new Gem[10];
@@ -14,6 +15,7 @@ public class Caravan {
     return inventory;
   }
   
+  // Adds gem to any empty slot -- *Note* Should excess be taken care of here?
   public void add(int val) {
     for(int i = 0; i < inventory.length; i++) {
       if(inventory[i] == null) {
@@ -28,12 +30,13 @@ public class Caravan {
     excess.add(new Gem(val));
   }
 
+  // Adds set of gems passed via parameter to the Caravan. If slots are full, add to excess
   public void add(GemSet gems) {
       int to_add = gems.size();
       int excess = 0;
 
-      if((size + to_add) > 10) {
-          excess = 10 - size - to_add;
+      if((size + to_add) > inventory.length) {
+          excess = inventory.length - size - to_add;
           to_add -= excess;
       }
 
@@ -55,11 +58,13 @@ public class Caravan {
       }    
   }
 
+  // Removes gem in specified slot
   public void remove(int slot) {
     inventory[slot] = null;
     size--;
   }
 
+  // Upgrades gem in specified slot
   public void upgrade(int slot) {
     inventory[slot].upgrade();
   }
